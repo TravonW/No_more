ad7f291d0ced8b05b77db413b20dea122ad2cb66
TXT-57 <travonwilliams321@gmail.com>
2023-06-27 16:41:47 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Logo from './components/logo';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const[count, setCount]= useState(0)
+
+  return (
+    <div id='base'>
+    <div id='head'> 
+    
+       <p>How many Yerbas Roxy Drinks {count}</p>
+         <button onClick={() => setCount(count + 1)}>
+        Forward 1 
+       </button>
+
+       <button onClick={() => setCount(count - 1)}>
+        Back 1 
+       </button>
+
+       <button onClick={() => setCount(count - count)}>
+        Reset
+       </button>
+
+
+
+      <h1> Goal 2 </h1>
+      <Logo/>
+       
+       
+
+
+
+    </div>
+</div>
+  );
+}
+
+export default App;
